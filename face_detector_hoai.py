4790318dcc2e2514c8dbb44232e9521d78d451c7
BachMiu <46811540+BachMiu@users.noreply.github.com>
2019-02-24 18:47:22 +0700

    Add files via upload

---
+++ face_detector_hoai.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2 as cv
+import sys
+
+TRAINING_FILE_NAME            = "haarcascade_frontalface_alt.xml"
+
+def showHelp():
+    print("Author: hoaint.13")
+    #print("--help/-h\tShow usage help")
+    print("usage: face_detector_hoai <image file name>")
+    print("Ex: face_detector_hoai demo.jpg")
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        showHelp()
+        exit(1)
+
+    imageFileName = sys.argv[1]
+    face_cascade = cv.CascadeClassifier(TRAINING_FILE_NAME)
+    img = cv.imread(imageFileName)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv.rectangle(img,(x,y),(x+w,y+h),(0, 0, 255),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+
+    faceFileName = "faces_" + imageFileName
+    cv.imwrite(faceFileName, img)
+    print ("Successfully saved the results to %s" %(faceFileName))
+    cv.imshow('img',img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
